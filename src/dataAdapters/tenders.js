847e30d1788793da2fd231055981b97ab8dff89e
PDS <skyseekerresearch@gmail.com>
2025-10-02 21:55:22 +0530

    Describe your change

---
+++ src/dataAdapters/tenders.js
@@ -0,0 +1,4 @@
+// src/tenders.js
+export * from "./dataAdapters/tenders.js";   // re-export everything from the real file
+// or, if you only need specific names:
+// export { generateCaption, reelScript } from "./dataAdapters/tenders.js";
